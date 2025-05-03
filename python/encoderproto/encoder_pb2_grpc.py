5bee5ee7a0471bec47071a70c316658705ea3de0
aveitsme <aveitsme@gmail.com>
2025-05-04 00:59:13 +0300

    package pb file patch

--- python/encoderproto/encoder_pb2_grpc.py
+++ python/encoderproto/encoder_pb2_grpc.py
@@ -3,7 +3,7 @@
 import grpc
 import warnings
 
-import encoder_pb2 as encoder__pb2
+from encoderproto import encoder_pb2 as encoder__pb2
 
 GRPC_GENERATED_VERSION = '1.71.0'
 GRPC_VERSION = grpc.__version__
